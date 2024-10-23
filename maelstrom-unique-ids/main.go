a5a6aaa1b0ff91c53fa7d5f64fa948b98f34c1e0
Yan Pitangui <yanpitangui@gmail.com>
2024-10-23 11:22:35 -0300

    Finished unique id generation

--- maelstrom-unique-ids/main.go
+++ maelstrom-unique-ids/main.go
@@ -1,8 +1,13 @@
 package main
 
 import (
+	"crypto/rand"
+	"encoding/base32"
+	"encoding/binary"
 	maelstrom "github.com/jepsen-io/maelstrom/demo/go"
 	"log"
+	"slices"
+	"time"
 )
 
 type GenerateResponse struct {
@@ -15,14 +20,34 @@ func main() {
 
 	n.Handle("generate", func(msg maelstrom.Message) error {
 
-		// TODO: actually implement some id generation
+		id, err := Generate()
+		if err != nil {
+			return err
+		}
+
 		return n.Reply(msg, &GenerateResponse{
 			Type: "generate_ok",
-			Id:   "whatever",
+			Id:   id,
 		})
 	})
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
+}
+
+func Generate() (string, error) {
+	nano := time.Now().UnixNano()
+	buf := make([]byte, binary.MaxVarintLen64)
+	n := binary.PutVarint(buf, nano)
+	timePart := buf[:n]
+	slices.Reverse(timePart)
+	randBytes := make([]byte, 6)
+	n, err := rand.Read(randBytes)
+	randPart := randBytes[:n]
+
+	if err != nil {
+		return "", err
+	}
+	return base32.StdEncoding.EncodeToString(append(timePart, randPart...)), nil
 }
